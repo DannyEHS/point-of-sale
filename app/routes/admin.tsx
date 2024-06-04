280bc4d8e0dcd608cc6ac7f3ee75d7fea61a84ef
Daniel Enrique Hernández Santana <dhernandezsantana1214@gmail.com>
2024-06-03 21:15:48 -0600

    ✨ Add user ui route

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,5 +1,15 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Fragment } from "react/jsx-runtime";
+import CardTopic from "~/components/ui/admin/CardTopic"
+
+import { FaRegUser, } from "react-icons/fa";
+import { CiBoxes } from "react-icons/ci";
+import { MdOutlineCategory, MdOutlineSecurity } from "react-icons/md";
+import { FaArrowUpRightFromSquare, FaRegNewspaper } from "react-icons/fa6";
+import { LuMonitorSmartphone } from "react-icons/lu";
+import { LiaBarcodeSolid } from "react-icons/lia";
+
+import { Link } from "@remix-run/react";
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,10 +20,86 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <Fragment>
-      <h1 className="text-[#3d3d3d] dark:text-white">Welcome to administration</h1>
-    </Fragment>
+    <div className="m-3 items-center justify-start h-screen w-full">
 
-    
+      <h1 className="mb-3 text-xl text-[#3d3d3d] dark:text-white">Welcome to administration</h1>
+
+      <div className="grid ">
+        <div className="grid grid-cols-3 gap-4 justify-center items-center p-2">
+          <Link to="/">
+            <CardTopic
+              textTitle="Usuarios"
+              textDescription="Aqui das de alta nuevos usuarios."
+              texContent="Manejo de Usuarios"
+              textFooter="Administración de usuarios"
+              iconContent={<FaRegUser className="mr-3" />}
+              iconFooter={<FaArrowUpRightFromSquare className="mr-3" />}
+            />
+          </Link>
+          <Link to="/">
+            <CardTopic
+              textTitle="Productos"
+              textDescription="Aqui das de alta nuevos productos."
+              texContent="Manejo de Productos"
+              textFooter="Agregar nuevo producto"
+              iconContent={<CiBoxes className="mr-3" />}
+              iconFooter={<FaArrowUpRightFromSquare className="mr-3" />}
+            />
+          </Link>
+          <Link to="/">
+            <CardTopic
+              textTitle="Categorias"
+              textDescription="Aqui das de alta nuevas categorias."
+              texContent="Manejo de Categorias"
+              textFooter="Agregar nueva categoria"
+              iconContent={<MdOutlineCategory className="mr-3" />}
+              iconFooter={<FaArrowUpRightFromSquare className="mr-3" />}
+            />
+          </Link>
+          <Link to="/">
+            <CardTopic
+              textTitle="Roles y Permisos"
+              textDescription="Aqui generas tus niveles de seguridad."
+              texContent="Manejo de Roles"
+              textFooter="Agregar nuevo rol"
+              iconContent={<MdOutlineSecurity className="mr-3" />}
+              iconFooter={<FaArrowUpRightFromSquare className="mr-3" />}
+            />
+          </Link>
+          <Link to="/">
+            <CardTopic
+              textTitle="Reportes"
+              textDescription="Aqui generas tus todos tus reportes."
+              texContent="Manejo de reportes."
+              textFooter="Revisar reportes"
+              iconContent={<FaRegNewspaper className="mr-3" />}
+              iconFooter={<FaArrowUpRightFromSquare className="mr-3" />}
+            />
+          </Link>
+          <Link to="/">
+            <CardTopic
+              textTitle="Editar app"
+              textDescription="Aqui cambias la estetica de la pagina."
+              texContent="Estilos y colores"
+              textFooter="Cambiar estilos"
+              iconContent={<LuMonitorSmartphone className="mr-3" />}
+              iconFooter={<FaArrowUpRightFromSquare className="mr-3" />}
+            />
+          </Link>
+          <Link to="/">
+            <CardTopic
+              textTitle="Generar codigo de barras"
+              textDescription="Aqui generas tus codigos de barras."
+              texContent="Codigo de barras"
+              textFooter="Crear codigo"
+              iconContent={<LiaBarcodeSolid className="mr-3" />}
+              iconFooter={<FaArrowUpRightFromSquare className="mr-3" />}
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+
+
   );
 }
