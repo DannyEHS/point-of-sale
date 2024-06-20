10e50821d8e899b8a26803a722bd8a5cf6bb2fb7
DannyEHS <117763569+DannyEHS@users.noreply.github.com>
2024-06-20 15:12:23 -0600

    ✨ Add action and loader to create categories (#25)

--- app/routes/createCategory.tsx
+++ app/routes/createCategory.tsx
@@ -1,5 +1,23 @@
 import FromCreateCategory from "~/components/ui/admin/categories/FormCreateCategory"
 
+import { createCategory } from "~/models/categories/createCategory"
+
+export const action = async ({ request } : ActionArgs) => {
+    const formData = await request.formData();
+    const name = await formData.get("name")
+    const description = await formData.get("description")
+
+    if(!name) throw new Error("Ingresa el nombre de la categoria.")
+    if(!description) throw new Error("Ingresa una descripcion para la categoria.")
+    
+    const newCategory = await createCategory({
+        name,
+        description,
+    })
+    console.log("Data de la categoria: ", newCategory);
+    return null;
+}
+
 export default function CreateCategory(){
     return(
         <div className="m-3 items-center justify-center h-screen w-full space-y-4">
