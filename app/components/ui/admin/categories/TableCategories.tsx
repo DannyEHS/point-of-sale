fb1e3b4e42b7ddf324d9d60d546423552b391b71
DannyEHS <117763569+DannyEHS@users.noreply.github.com>
2024-06-20 13:41:42 -0600

    ✨ Add category route (#23)

---
+++ app/components/ui/admin/categories/TableCategories.tsx
@@ -0,0 +1,69 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "~/components/ui/table"
+
+import DeleteButton from "../../general/DeleteButton";
+import EditButton from "../../general/EditButton";
+import TooltipInfo from "../../general/TooltipInfo"
+import { Form } from "@remix-run/react";
+
+type Props = {
+    data?: string | any
+}
+
+export default function TableCategories({ data }: Props) {
+    return (
+        <Table className="border border-[#d1d1d1]">
+            <TableCaption>Lista de tus Categorias.</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="w-[100px]">ID</TableHead>
+                    <TableHead>Categoria</TableHead>
+                    <TableHead>Descripcion</TableHead>
+                    <TableHead>Accion</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {data && data.map((rol: any) => (
+                    <TableRow key={rol.id}>
+                        <TableCell className="font-medium">{rol.id}</TableCell>
+                        <TableCell>{rol.name}</TableCell>
+                        <TableCell>{rol.description}</TableCell>
+                        <TableCell className="flex flex-row space-x-2">
+                            <TooltipInfo
+                                element={<EditButton route={`/editRol/${rol.id}`} />}
+                                text="Editar categoria"
+                            />
+                            <TooltipInfo
+                                element={
+                                    <Form method="post">
+                                        <DeleteButton catchId={rol.id} />
+                                    </Form>
+                                    
+                                }
+                                text="Eliminar categoria"
+                            />
+                        </TableCell>
+                    </TableRow>
+                ))}
+                {
+                    !data ? (
+                        <TableRow >
+                            <TableCell className="font-medium">Any data</TableCell>
+                            <TableCell>Any data</TableCell>
+                            <TableCell>Any data</TableCell>
+                            <TableCell>Any data</TableCell>
+                        </TableRow>
+                    ) : null
+                }
+            </TableBody>
+        </Table>
+    )
+}
---
+++ app/routes/admin_.categories.tsx
@@ -0,0 +1,24 @@
+import { Input } from "~/components/ui/input"
+import { Link } from "@remix-run/react";
+import { Button } from "~/components/ui/button"
+import TableCategories from "~/components/ui/admin/categories/TableCategories"
+
+export default function Categories() {
+
+    return (
+        <div className="m-3 items-center justify-start h-screen w-full space-y-4">
+            <h1 className="mb-3 text-xl text-[#3d3d3d] dark:text-white">
+                Categorias
+            </h1>
+            <div className="flex flex-row w-full items-center space-x-2">
+                <Input className="w-96" placeholder="Buscar categoria" />
+                <Link to="/">
+                    <Button>Crear Categoria</Button>
+                </Link>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+                <TableCategories/>
+            </div>
+        </div>
+    );
+}
