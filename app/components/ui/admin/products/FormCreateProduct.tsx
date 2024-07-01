189eca448bf475b9e95a8be5fd843b8780113368
Daniel Enrique Hernández Santana <dhernandezsantana1214@gmail.com>
2024-07-01 00:26:17 -0600

    ✨ feat-add-action-loader-for-create-products

---
+++ app/components/ui/admin/products/FormCreateProduct.tsx
@@ -0,0 +1,90 @@
+import { Form, Link } from "@remix-run/react";
+import { Input } from "~/components/ui/input"
+import { Button } from "~/components/ui/button"
+import { Textarea } from "~/components/ui/textarea"
+import TooltipInfo from "~/components/ui/general/TooltipInfo"
+import { IoMdHelpCircleOutline } from "react-icons/io";
+import SelectItems from "~/components/ui/admin/SelectItems";
+
+type Props = {
+    categoryData: {
+        id: string;
+        name: string;
+    }[];
+}
+
+export default function FromCreateCategory({ categoryData } : Props) {
+    console.log(categoryData);
+    return (
+        <>
+            <Form method="post" className="flex flex-col items-center bg-[#fafbfb] dark:bg-[#252525] w-9/12">
+                <div className="flex flex-row items-center">
+                    <Input name="barCode" className=" w-96 m-3 " placeholder="Codigo de barras" />
+                    <TooltipInfo
+                        element={<IoMdHelpCircleOutline />}
+                        text="Aqui ingresas el codigo de barras de tus productos"
+                    />
+                </div>
+                <div className="flex flex-row items-center">
+                    <Input name="name" className=" w-96 m-3 " placeholder="Nombre del producto" />
+                    <TooltipInfo
+                        element={<IoMdHelpCircleOutline />}
+                        text="Aqui ingresa el nombre del product a crear."
+                    />
+                </div>
+                <div className="flex flex-row items-center">
+                    <Input name="stock" className=" w-96 m-3 " placeholder="Cantidad" />
+                    <TooltipInfo
+                        element={<IoMdHelpCircleOutline />}
+                        text="Aqui ingresa la cantidad de productos a ingresar."
+                    />
+                </div>
+                <div className="flex flex-row items-center">
+                    <Input name="price" className=" w-96 m-3 " placeholder="Ingresa el precio" />
+                    <TooltipInfo
+                        element={<IoMdHelpCircleOutline />}
+                        text="Aqui ingresas el precio al que daras el producto."
+                    />
+                </div>
+                <div className="flex flex-row items-center">
+                    <Input name="cost" className=" w-96 m-3 " placeholder="Ingresa el costo" />
+                    <TooltipInfo
+                        element={<IoMdHelpCircleOutline />}
+                        text="Aqui ingresa lo que te costo comprar dicho producto a tu proveedor."
+                    />
+                </div>
+                <div className="flex flex-row items-center">
+                    <Textarea name="description" className=" w-96 m-3 " placeholder="Ingresa un breve descripcion" />
+                    <TooltipInfo
+                        element={<IoMdHelpCircleOutline />}
+                        text="Aqui ingresa una descripcion que haga referencia al producto que desees crear."
+                    />
+                </div>
+                
+                <div className="flex flex-row items-center">
+                    <SelectItems name="category" placeHolder="Categorias" data={categoryData}/>
+                    <TooltipInfo
+                        element={<IoMdHelpCircleOutline />}
+                        text="Aqui seleccionas la categoria a la que debe pertenecer el producto."
+                    />
+                </div>
+                <div className="flex flex-row space-x-3 m-3 items-center">
+                    <TooltipInfo
+                        element={
+                            <Link to="/admin/categories">
+                                <Button>Regresar</Button>
+                            </Link>
+                        }
+                        text="Presiona el boton para regresar al listado de categorias."
+                    />
+                    <TooltipInfo
+                        element={
+                            <Button type="submit">Crear</Button>
+                        }
+                        text="Si los campos estan completos, puedes crear tu categoria al presionar este boton."
+                    />
+                </div>
+            </Form>
+        </>
+    )
+}
--- app/routes/admin_.products.tsx
+++ app/routes/admin_.products.tsx
@@ -1,10 +1,28 @@
 import { Input } from "~/components/ui/input"
 import { Button } from "~/components/ui/button"
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import TableProducts from "~/components/ui/admin/products/TableProducts";
+import prisma from "prisma/prisma";
+
+export const loader = async () => {
+    const categories = await prisma.category.findMany();
+    const productsAndCategories = await prisma.product.findMany({
+        include: {
+            category: true,
+        },
+    });
+    
+    console.log(productsAndCategories)
+    return{
+        products: productsAndCategories
+    }
+}
 
 
 export default function Products() {
+
+    const { products } = useLoaderData<typeof loader>();
+
     return(
         <div className="m-3 items-center justify-start h-screen w-full space-y-4">
             <h1 className="mb-3 text-xl text-[#3d3d3d] dark:text-white">
@@ -12,12 +30,12 @@ export default function Products() {
             </h1>
             <div className="flex flex-row w-full items-center space-x-2">
                 <Input className="w-96" placeholder="Buscar usuario" />
-                <Link to="/createRol">
-                    <Button>Apachurrame</Button>
+                <Link to="/createProducts">
+                    <Button>Crear Producto</Button>
                 </Link>
             </div>
             <div className="flex flex-col items-center justify-center">
-                <TableProducts />
+                <TableProducts data={products}/>
             </div>
         </div>
     )
