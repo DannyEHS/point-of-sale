8f1bce3f4361f650104505b737c43f6f34b48280
Daniel Payan <dany.sasket@gmail.com>
2024-07-17 15:59:26 -0600

    :lipstick: improve-ui-sidebar

--- app/components/ui/Navigation/routes.ts
+++ app/components/ui/Navigation/routes.ts
@@ -4,6 +4,7 @@ type Route = {
     name: string
     path: string
     icon: IconName
+    sub?: Route[]
 }
 
 export const routes: Route[] = [
@@ -13,19 +14,52 @@ export const routes: Route[] = [
         icon: 'home',
     },
     {
-        name: 'POS',
+        name: 'Punto de Venta',
         path: '/pos',
         icon: 'cart',
     },
     {
-        name: 'Inventory',
+        name: 'Inventario',
         path: '/inventory',
         icon: 'inventory',
     },
     {
-        name: 'Administration',
+        name: 'Administración',
         path: '/admin',
         icon: 'admin',
+        sub: [
+            {
+                name: 'Usuarios',
+                path: '/admin/users',
+                icon: 'user',
+            },
+            {
+                name: 'Productos',
+                path: '/admin/products',
+                icon: 'product',
+            },
+            {
+                name: 'Categorias',
+                path: '/admin/categories',
+                icon: 'category',
+            },
+            {
+                name: 'Roles y permisos',
+                path: '/admin/rol',
+                icon: 'rol',
+            },
+            {
+                name: 'Reportes',
+                path: '/admin/report',
+                icon: 'report',
+            },
+            {
+                name: 'Código de barras',
+                path: '/admin/code',
+                icon: 'code',
+            },
+
+        ]
     }
 
 ]
--- app/components/ui/Navigation/sideBar.tsx
+++ app/components/ui/Navigation/sideBar.tsx
@@ -3,30 +3,59 @@ import { Link } from '@remix-run/react'
 import { routes } from './routes'
 import { Button } from '../button'
 import { IconComponent } from '../icon'
+import { useLocation } from 'react-router-dom'
+import {Fragment} from 'react/jsx-runtime'
+import { FaAngleDown } from "react-icons/fa";
+import { FaAngleUp } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function SideBar() {
+    const location = useLocation();
+    const isActive = (path: string) => location.pathname === path;
+    
     return (
-        <aside className="flex flex-col w-60 items-center bg-[#fafbfb] border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
-            <div className='sticky top-0 z-50 flex h-[60px] items-center border-b px-6 bg-[#fafbfb]'>
+        <aside className="flex flex-col w-56 items-center bg-[#fafbfb] border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
+            <div className='sticky top-0 z-50 flex h-[60px] items-center border-b px-4 bg-[#fafbfb]'>
                 <Link className='flex items-center gap-2 font-semibold' key='/' to='/'>
                     {/* Icono */}
-                    <span className='text-[#3d3d3d] dark:text-white'>Acme POS</span>
+                    <Button className='ml-auto' variant='ghost'><GiHamburgerMenu className='size-4'/></Button>
+                    <span className='text-[#3d3d3d] dark:text-white'>GPI PymeX</span>
                 </Link>
-                <Button className='ml-auto' variant='outline'>---</Button>
+                
             </div>
             <ul className="sticky top-16 z-50 flex flex-col px-4 pt-4 text-sm font-medium">
                 {routes.map((route) => (
-                    <li key={route.path} className="flex items-center gap-3 ">
+                    <Fragment key={route.path}>
+                        <li key={route.path} className="flex items-center gap-3 ">
+                            
+                            <Link 
+                                key={route.path}
+                                to={route.path}
+                                className={isActive(route.path) ? 
+                                    'flex items-center gap-3 rounded-lg px-3 py-2 w-full text-gray-900 transition-all bg-gray-100 dark:bg-gray-400 dark:text-gray-50 dark:hover:text-gray-50' 
+                                    : 
+                                    'flex items-center gap-3 rounded-lg px-3 py-2 w-full text-gray-500 transition-all hover:bg-gray-100 hover:text-gray-900 dark:bg-gray-400 dark:text-gray-50 dark:hover:text-gray-50'}
+                            >
+                                <IconComponent value={route.icon} />
+                                {route.name}
+                                {route.sub && <span className='ml-auto hover:bg-gray-200 p-1 rounded-md'><FaAngleDown /></span>}
+                            </Link>
+
+                        </li>
+                        {route.sub && route.sub.map((subRoute) => (
+                            <li className=''>
+                                <Link
+                                    key={subRoute.path}
+                                    to={subRoute.path}
+                                    className='pl-6 flex items-center  gap-2 rounded-lg px-3 py-2 w-full text-gray-500 transition-all hover:bg-gray-100 hover:text-gray-900 dark:bg-gray-400 dark:text-gray-50 dark:hover:text-gray-50'
+                                >
+                                    <IconComponent value={subRoute.icon}/>
+                                    {subRoute.name}
+                                </Link>   
+                            </li> 
+                        ))}
                         
-                        <Link 
-                            key={route.path}
-                            to={route.path}
-                            className='flex items-center gap-3 rounded-lg px-3 py-2 w-full text-gray-500 transition-all hover:bg-gray-100 hover:text-gray-900 dark:bg-gray-400 dark:text-gray-50 dark:hover:text-gray-50'
-                        >
-                            <IconComponent value={route.icon}/>
-                            {route.name}
-                        </Link>
-                    </li>
+                    </Fragment>
                 ))}
             </ul>
         </aside>
